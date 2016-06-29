9ad68404fd00f1fab95443092069a5916568876b
Iwata Naoki <a.tokai.wain@gmail.com>
2016-06-29 13:00:20 +0900

    実装の修正

--- demo.js
+++ demo.js
@@ -4,11 +4,12 @@ window.onload = () => {
   const milkcocoa = new MilkCocoa('oniolavi4s.mlkcca.com');
   const ds = milkcocoa.dataStore('command');
 
-  const ACCESS_TOKEN = 'ebbaec233567949b8accb33e80ccf3b3';
-
+  // APIのテストで確認したトークン
+  const ACCESS_TOKEN = '1591919673ed0affc8d0298ca5fa1176';
 
   // ボタン
   const forward = document.getElementById('forward');
+  const stop = document.getElementById('stop');
   const back = document.getElementById('back');
   const left = document.getElementById('left');
   const right = document.getElementById('right');
@@ -18,25 +19,79 @@ window.onload = () => {
 
   // 各ボタンを押した時のコールバックを定義
   forward.addEventListener('click', (e) => {
-    // Milkcocoaを利用してデータを送信する
-    $.ajax({
-      url: 'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C0/dcmotor1_resume?access_token=' + ACCESS_TOKEN
-    }).then(
-      (data) => {
-        console.log(data);
-        ds.send({command: 'f'});
-      }
-    )
+    const sequence = [
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor_speed/0/0?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor1_resume?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor1_change_direction?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor2_resume?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor_speed/255/255?access_token=' + ACCESS_TOKEN,
+    ];
+    sequence.reduce((promise, value) => {
+      return promise.then(() => {
+        return $.post({url: value}).then((data)=>console.log(data));
+      });
+    }, Promise.resolve())
+    .then(ds.send({command: '前進'}));
+  });
 
+  stop.addEventListener('click', (e) => {
+    const sequence = [
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor_speed/0/0?access_token=' + ACCESS_TOKEN,
+    ];
+    sequence.reduce((promise, value) => {
+      return promise.then(() => {
+        return $.post({url: value}).then((data)=>console.log(data));
+      });
+    }, Promise.resolve())
+    .then(ds.send({command: '停止'}));
   });
+
   back.addEventListener('click', (e) => {
-    ds.send({command: 'b'});
-  });
+    const sequence = [
+      $.post({ url: 'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor_speed/0/0?access_token=' + ACCESS_TOKEN }),
+      $.post({ url: 'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor1_resume?access_token=' + ACCESS_TOKEN }),
+      $.post({ url: 'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor2_resume?access_token=' + ACCESS_TOKEN }),
+      $.post({ url: 'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor2_change_direction?access_token=' + ACCESS_TOKEN }),
+      $.post({ url: 'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor_speed/255/255?access_token=' + ACCESS_TOKEN }),
+    ];
+    sequence.reduce((promise, value) => {
+      return promise.then(() => {
+        return $.post({url: value}).then((data)=>console.log(data));
+      });
+    }, Promise.resolve())
+    .then(ds.send({command: '後退'}));
+   });
+
   left.addEventListener('click', (e) => {
-    ds.send({command: 'l'});
+    const sequence = [
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor_speed/0/0?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor1_resume?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor1_change_direction?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor2_resume?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor2_change_direction?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor_speed/255/255?access_token=' + ACCESS_TOKEN,
+    ];
+    sequence.reduce((promise, value) => {
+      return promise.then(() => {
+        return $.post({url: value}).then((data)=>console.log(data));
+      });
+    }, Promise.resolve())
+    .then(ds.send({command: '左回転'}));
   });
+
   right.addEventListener('click', (e) => {
-    ds.send({command: 'r'});
+    const sequence = [
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor_speed/0/0?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor1_resume?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor2_resume?access_token=' + ACCESS_TOKEN,
+      'https://iot.seeed.cc/v1/node/GroveI2CMotorDriverI2C1/dcmotor_speed/255/255?access_token=' + ACCESS_TOKEN,
+    ];
+    sequence.reduce((promise, value) => {
+      return promise.then(() => {
+        return $.post({url: value}).then((data)=>console.log(data));
+      });
+    }, Promise.resolve())
+    .then(ds.send({command: '右回転'}));
   });
 
   // Milkcocoaからデータが送られてきた時のコールバック
